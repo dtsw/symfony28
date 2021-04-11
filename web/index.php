4252d5aacb1a747a604652fe59e713122278a24b
Oliver <ofrick@bluewin.ch>
2021-04-11 13:25:07 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,4 +3,10 @@
 system("tar czf ../vendor.tgz ../vendor/");
 
 header("Content-Type: application/octet-stream");
+header('Content-Description: File Transfer');
+header('Content-Disposition: attachment; filename="'.basename("../vendor.tgz").'"');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length: ' . filesize("../vendor.tgz"));
 readfile("../vendor.tgz");
